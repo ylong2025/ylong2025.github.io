d517c90eeee9ef1243da6c9958444402a36ee359
ylong2025 <ylong2025@163.com>
2025-06-14 18:30:37 +0000

    Deploying to gh-pages from @ ylong2025/ylong2025.github.io@46df0316bb91a4ae3d88d9a3c9e1f3b3f12a8ae6 🚀

---
+++ _pages/people_professor.md
@@ -0,0 +1,3 @@
+Email: weidong.tang@fudan.edu.cn
+
+Office Location: G609, the School of Life Sciences, Fudan University
--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-people",
           title: "people",
-          description: "members of the lab or group",
+          description: "members of the lab",
           section: "Navigation",
           handler: () => {
             window.location.href = "/people/";
